c165415ca4b5fb9b70e40746a98f6b7828804a29
Carry <864140438@qq.com>
2020-11-06 14:09:49 +0800

    code format

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
-#-------------------------------------#
+# -------------------------------------#
 #       对数据集进行训练
-#-------------------------------------#
+# -------------------------------------#
 import os
 import numpy as np
 import time
@@ -12,13 +12,14 @@ import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
 from utils.dataloader import yolo_dataset_collate, YoloDataset
-from nets.yolo_training import YOLOLoss,Generator
+from nets.yolo_training import YOLOLoss, Generator
 from nets.yolo4 import YoloBody
 from tqdm import tqdm
 
-#---------------------------------------------------#
+
+# ---------------------------------------------------#
 #   获得类和先验框
-#---------------------------------------------------#
+# ---------------------------------------------------#
 def get_classes(classes_path):
     '''loads the classes'''
     with open(classes_path) as f:
@@ -26,22 +27,25 @@ def get_classes(classes_path):
     class_names = [c.strip() for c in class_names]
     return class_names
 
+
 def get_anchors(anchors_path):
     '''loads the anchors from a file'''
     with open(anchors_path) as f:
         anchors = f.readline()
     anchors = [float(x) for x in anchors.split(',')]
-    return np.array(anchors).reshape([-1,3,2])[::-1,:,:]
+    return np.array(anchors).reshape([-1, 3, 2])[::-1, :, :]
+
 
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
-def fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,genval,Epoch,cuda):
+
+def fit_one_epoch(net, yolo_losses, epoch, epoch_size, epoch_size_val, gen, genval, Epoch, cuda):
     total_loss = 0
     val_loss = 0
     start_time = time.time()
-    with tqdm(total=epoch_size,desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
+    with tqdm(total=epoch_size, desc=f'Epoch {epoch + 1}/{Epoch}', postfix=dict, mininterval=0.3) as pbar:
         for iteration, batch in enumerate(gen):
             if iteration >= epoch_size:
                 break
@@ -65,16 +69,16 @@ def fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,genval,Epo
 
             total_loss += loss
             waste_time = time.time() - start_time
-            
-            pbar.set_postfix(**{'total_loss': total_loss.item() / (iteration + 1), 
-                                'lr'        : get_lr(optimizer),
-                                'step/s'    : waste_time})
+
+            pbar.set_postfix(**{'total_loss': total_loss.item() / (iteration + 1),
+                                'lr': get_lr(optimizer),
+                                'step/s': waste_time})
             pbar.update(1)
 
             start_time = time.time()
 
     print('Start Validation')
-    with tqdm(total=epoch_size_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
+    with tqdm(total=epoch_size_val, desc=f'Epoch {epoch + 1}/{Epoch}', postfix=dict, mininterval=0.3) as pbar:
         for iteration, batch in enumerate(genval):
             if iteration >= epoch_size_val:
                 break
@@ -99,58 +103,60 @@ def fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,genval,Epo
             pbar.update(1)
 
     print('Finish Validation')
-    print('Epoch:'+ str(epoch+1) + '/' + str(Epoch))
-    print('Total Loss: %.4f || Val Loss: %.4f ' % (total_loss/(epoch_size+1),val_loss/(epoch_size_val+1)))
+    print('Epoch:' + str(epoch + 1) + '/' + str(Epoch))
+    print('Total Loss: %.4f || Val Loss: %.4f ' % (total_loss / (epoch_size + 1), val_loss / (epoch_size_val + 1)))
 
-    print('Saving state, iter:', str(epoch+1))
-    torch.save(model.state_dict(), 'logs/Epoch%d-Total_Loss%.4f-Val_Loss%.4f.pth'%((epoch+1),total_loss/(epoch_size+1),val_loss/(epoch_size_val+1)))
+    print('Saving state, iter:', str(epoch + 1))
+    torch.save(model.state_dict(), 'logs/Epoch%d-Total_Loss%.4f-Val_Loss%.4f.pth' % (
+    (epoch + 1), total_loss / (epoch_size + 1), val_loss / (epoch_size_val + 1)))
 
-#----------------------------------------------------#
+
+# ----------------------------------------------------#
 #   检测精度mAP和pr曲线计算参考视频
 #   https://www.bilibili.com/video/BV1zE411u7Vw
-#----------------------------------------------------#
+# ----------------------------------------------------#
 if __name__ == "__main__":
-    #-------------------------------#
+    # -------------------------------#
     #   输入的shape大小
     #   显存比较小可以使用416x416
     #   显存比较大可以使用608x608
-    #-------------------------------#
-    input_shape = (416,416)
-    #-------------------------------#
+    # -------------------------------#
+    input_shape = (416, 416)
+    # -------------------------------#
     #   tricks的使用设置
-    #-------------------------------#
+    # -------------------------------#
     Cosine_lr = False
     mosaic = True
     # 用于设定是否使用cuda
     Cuda = True
     smoooth_label = 0
-    #-------------------------------#
+    # -------------------------------#
     #   Dataloder的使用
-    #-------------------------------#
+    # -------------------------------#
     Use_Data_Loader = True
 
     annotation_path = '2007_train.txt'
-    #-------------------------------#
+    # -------------------------------#
     #   获得先验框和类
-    #-------------------------------#
+    # -------------------------------#
     anchors_path = 'model_data/yolo_anchors.txt'
-    classes_path = 'model_data/voc_classes.txt'   
+    classes_path = 'model_data/voc_classes.txt'
     class_names = get_classes(classes_path)
     anchors = get_anchors(anchors_path)
     num_classes = len(class_names)
-    
+
     # 创建模型
-    model = YoloBody(len(anchors[0]),num_classes)
-    #-------------------------------------------#
+    model = YoloBody(len(anchors[0]), num_classes)
+    # -------------------------------------------#
     #   权值文件的下载请看README
-    #-------------------------------------------#
+    # -------------------------------------------#
     model_path = "model_data/yolo4_weights.pth"
     # 加快模型训练的效率
     print('Loading weights into state dict...')
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model_dict = model.state_dict()
     pretrained_dict = torch.load(model_path, map_location=device)
-    pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) ==  np.shape(v)}
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) == np.shape(v)}
     model_dict.update(pretrained_dict)
     model.load_state_dict(model_dict)
     print('Finished!')
@@ -165,8 +171,8 @@ if __name__ == "__main__":
     # 建立loss函数
     yolo_losses = []
     for i in range(3):
-        yolo_losses.append(YOLOLoss(np.reshape(anchors,[-1,2]),num_classes, \
-                                (input_shape[1], input_shape[0]), smoooth_label, Cuda))
+        yolo_losses.append(YOLOLoss(np.reshape(anchors, [-1, 2]), num_classes, \
+                                    (input_shape[1], input_shape[0]), smoooth_label, Cuda))
 
     # 0.1用于验证，0.9用于训练
     val_split = 0.1
@@ -175,52 +181,52 @@ if __name__ == "__main__":
     np.random.seed(10101)
     np.random.shuffle(lines)
     np.random.seed(None)
-    num_val = int(len(lines)*val_split)
+    num_val = int(len(lines) * val_split)
     num_train = len(lines) - num_val
-    
-    #------------------------------------------------------#
+
+    # ------------------------------------------------------#
     #   主干特征提取网络特征通用，冻结训练可以加快训练速度
     #   也可以在训练初期防止权值被破坏。
     #   Init_Epoch为起始世代
     #   Freeze_Epoch为冻结训练的世代
     #   Epoch总训练世代
     #   提示OOM或者显存不足请调小Batch_size
-    #------------------------------------------------------#
+    # ------------------------------------------------------#
     if True:
         lr = 1e-3
         Batch_size = 4
         Init_Epoch = 0
         Freeze_Epoch = 50
-        
-        optimizer = optim.Adam(net.parameters(),lr,weight_decay=5e-4)
+
+        optimizer = optim.Adam(net.parameters(), lr, weight_decay=5e-4)
         if Cosine_lr:
             lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5, eta_min=1e-5)
         else:
-            lr_scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=1,gamma=0.95)
+            lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
 
         if Use_Data_Loader:
             train_dataset = YoloDataset(lines[:num_train], (input_shape[0], input_shape[1]), mosaic=mosaic)
             val_dataset = YoloDataset(lines[num_train:], (input_shape[0], input_shape[1]), mosaic=False)
             gen = DataLoader(train_dataset, batch_size=Batch_size, num_workers=4, pin_memory=True,
-                                    drop_last=True, collate_fn=yolo_dataset_collate)
-            gen_val = DataLoader(val_dataset, batch_size=Batch_size, num_workers=4,pin_memory=True, 
-                                    drop_last=True, collate_fn=yolo_dataset_collate)
+                             drop_last=True, collate_fn=yolo_dataset_collate)
+            gen_val = DataLoader(val_dataset, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                                 drop_last=True, collate_fn=yolo_dataset_collate)
         else:
             gen = Generator(Batch_size, lines[:num_train],
-                            (input_shape[0], input_shape[1])).generate(mosaic = mosaic)
+                            (input_shape[0], input_shape[1])).generate(mosaic=mosaic)
             gen_val = Generator(Batch_size, lines[num_train:],
-                            (input_shape[0], input_shape[1])).generate(mosaic = False)
+                                (input_shape[0], input_shape[1])).generate(mosaic=False)
 
-        epoch_size = max(1, num_train//Batch_size)
-        epoch_size_val = num_val//Batch_size
-        #------------------------------------#
+        epoch_size = max(1, num_train // Batch_size)
+        epoch_size_val = num_val // Batch_size
+        # ------------------------------------#
         #   冻结一定部分训练
-        #------------------------------------#
+        # ------------------------------------#
         for param in model.backbone.parameters():
             param.requires_grad = False
 
-        for epoch in range(Init_Epoch,Freeze_Epoch):
-            fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,gen_val,Freeze_Epoch,Cuda)
+        for epoch in range(Init_Epoch, Freeze_Epoch):
+            fit_one_epoch(net, yolo_losses, epoch, epoch_size, epoch_size_val, gen, gen_val, Freeze_Epoch, Cuda)
             lr_scheduler.step()
 
     if True:
@@ -229,33 +235,33 @@ if __name__ == "__main__":
         Freeze_Epoch = 50
         Unfreeze_Epoch = 100
 
-        optimizer = optim.Adam(net.parameters(),lr,weight_decay=5e-4)
+        optimizer = optim.Adam(net.parameters(), lr, weight_decay=5e-4)
         if Cosine_lr:
             lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5, eta_min=1e-5)
         else:
-            lr_scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=1,gamma=0.95)
+            lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
 
         if Use_Data_Loader:
             train_dataset = YoloDataset(lines[:num_train], (input_shape[0], input_shape[1]), mosaic=mosaic)
             val_dataset = YoloDataset(lines[num_train:], (input_shape[0], input_shape[1]), mosaic=False)
             gen = DataLoader(train_dataset, batch_size=Batch_size, num_workers=4, pin_memory=True,
-                                    drop_last=True, collate_fn=yolo_dataset_collate)
-            gen_val = DataLoader(val_dataset, batch_size=Batch_size, num_workers=4,pin_memory=True, 
-                                    drop_last=True, collate_fn=yolo_dataset_collate)
+                             drop_last=True, collate_fn=yolo_dataset_collate)
+            gen_val = DataLoader(val_dataset, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                                 drop_last=True, collate_fn=yolo_dataset_collate)
         else:
             gen = Generator(Batch_size, lines[:num_train],
-                            (input_shape[0], input_shape[1])).generate(mosaic = mosaic)
+                            (input_shape[0], input_shape[1])).generate(mosaic=mosaic)
             gen_val = Generator(Batch_size, lines[num_train:],
-                            (input_shape[0], input_shape[1])).generate(mosaic = False)
+                                (input_shape[0], input_shape[1])).generate(mosaic=False)
 
-        epoch_size = max(1, num_train//Batch_size)
-        epoch_size_val = num_val//Batch_size
-        #------------------------------------#
+        epoch_size = max(1, num_train // Batch_size)
+        epoch_size_val = num_val // Batch_size
+        # ------------------------------------#
         #   解冻后训练
-        #------------------------------------#
+        # ------------------------------------#
         for param in model.backbone.parameters():
             param.requires_grad = True
 
-        for epoch in range(Freeze_Epoch,Unfreeze_Epoch):
-            fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,gen_val,Unfreeze_Epoch,Cuda)
+        for epoch in range(Freeze_Epoch, Unfreeze_Epoch):
+            fit_one_epoch(net, yolo_losses, epoch, epoch_size, epoch_size_val, gen, gen_val, Unfreeze_Epoch, Cuda)
             lr_scheduler.step()
--- yolo.py
+++ yolo.py
@@ -1,6 +1,6 @@
-#-------------------------------------#
+# -------------------------------------#
 #       创建YOLO类
-#-------------------------------------#
+# -------------------------------------#
 import cv2
 import numpy as np
 import colorsys
@@ -9,22 +9,23 @@ import torch
 import torch.nn as nn
 from nets.yolo4 import YoloBody
 import torch.backends.cudnn as cudnn
-from PIL import Image,ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDraw
 from torch.autograd import Variable
-from utils.utils import non_max_suppression, bbox_iou, DecodeBox,letterbox_image,yolo_correct_boxes
+from utils.utils import non_max_suppression, bbox_iou, DecodeBox, letterbox_image, yolo_correct_boxes
 
-#--------------------------------------------#
+
+# --------------------------------------------#
 #   使用自己训练好的模型预测需要修改2个参数
 #   model_path和classes_path都需要修改！
-#--------------------------------------------#
+# --------------------------------------------#
 class YOLO(object):
     _defaults = {
         "model_path": 'model_data/yolo4_weights.pth',
         "anchors_path": 'model_data/yolo_anchors.txt',
         "classes_path": 'model_data/coco_classes.txt',
-        "model_image_size" : (416, 416, 3),
+        "model_image_size": (416, 416, 3),
         "confidence": 0.5,
-        "iou" : 0.3,
+        "iou": 0.3,
         "cuda": True
     }
 
@@ -35,58 +36,59 @@ class YOLO(object):
         else:
             return "Unrecognized attribute name '" + n + "'"
 
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     #   初始化YOLO
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
         self.class_names = self._get_class()
         self.anchors = self._get_anchors()
         self.generate()
-    #---------------------------------------------------#
+
+    # ---------------------------------------------------#
     #   获得所有的分类
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def _get_class(self):
         classes_path = os.path.expanduser(self.classes_path)
         with open(classes_path) as f:
             class_names = f.readlines()
         class_names = [c.strip() for c in class_names]
         return class_names
-    
-    #---------------------------------------------------#
+
+    # ---------------------------------------------------#
     #   获得所有的先验框
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def _get_anchors(self):
         anchors_path = os.path.expanduser(self.anchors_path)
         with open(anchors_path) as f:
             anchors = f.readline()
         anchors = [float(x) for x in anchors.split(',')]
-        return np.array(anchors).reshape([-1, 3, 2])[::-1,:,:]
+        return np.array(anchors).reshape([-1, 3, 2])[::-1, :, :]
 
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     #   获得所有的分类
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def generate(self):
-        
-        self.net = YoloBody(len(self.anchors[0]),len(self.class_names)).eval()
+
+        self.net = YoloBody(len(self.anchors[0]), len(self.class_names)).eval()
 
         # 加快模型训练的效率
         print('Loading weights into state dict...')
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         state_dict = torch.load(self.model_path, map_location=device)
         self.net.load_state_dict(state_dict)
-        
+
         if self.cuda:
             os.environ["CUDA_VISIBLE_DEVICES"] = '0'
             self.net = nn.DataParallel(self.net)
             self.net = self.net.cuda()
-    
+
         print('Finished!')
 
         self.yolo_decodes = []
         for i in range(3):
-            self.yolo_decodes.append(DecodeBox(self.anchors[i], len(self.class_names),  (self.model_image_size[1], self.model_image_size[0])))
-
+            self.yolo_decodes.append(
+                DecodeBox(self.anchors[i], len(self.class_names), (self.model_image_size[1], self.model_image_size[0])))
 
         print('{} model, anchors, and classes loaded.'.format(self.model_path))
         # 画框设置不同的颜色
@@ -97,14 +99,14 @@ class YOLO(object):
             map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
                 self.colors))
 
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     #   检测图片
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def detect_image(self, image):
         image_shape = np.array(np.shape(image)[0:2])
 
-        crop_img = np.array(letterbox_image(image, (self.model_image_size[0],self.model_image_size[1])))
-        photo = np.array(crop_img,dtype = np.float32)
+        crop_img = np.array(letterbox_image(image, (self.model_image_size[0], self.model_image_size[1])))
+        photo = np.array(crop_img, dtype=np.float32)
         photo /= 255.0
         photo = np.transpose(photo, (2, 0, 1))
         photo = photo.astype(np.float32)
@@ -117,29 +119,33 @@ class YOLO(object):
             if self.cuda:
                 images = images.cuda()
             outputs = self.net(images)
-            
+
         output_list = []
         for i in range(3):
             output_list.append(self.yolo_decodes[i](outputs[i]))
         output = torch.cat(output_list, 1)
         batch_detections = non_max_suppression(output, len(self.class_names),
-                                                conf_thres=self.confidence,
-                                                nms_thres=self.iou)
+                                               conf_thres=self.confidence,
+                                               nms_thres=self.iou)
         try:
             batch_detections = batch_detections[0].cpu().numpy()
         except:
             return image
-            
-        top_index = batch_detections[:,4]*batch_detections[:,5] > self.confidence
-        top_conf = batch_detections[top_index,4]*batch_detections[top_index,5]
-        top_label = np.array(batch_detections[top_index,-1],np.int32)
-        top_bboxes = np.array(batch_detections[top_index,:4])
-        top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(top_bboxes[:,0],-1),np.expand_dims(top_bboxes[:,1],-1),np.expand_dims(top_bboxes[:,2],-1),np.expand_dims(top_bboxes[:,3],-1)
+
+        top_index = batch_detections[:, 4] * batch_detections[:, 5] > self.confidence
+        top_conf = batch_detections[top_index, 4] * batch_detections[top_index, 5]
+        top_label = np.array(batch_detections[top_index, -1], np.int32)
+        top_bboxes = np.array(batch_detections[top_index, :4])
+        top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(top_bboxes[:, 0], -1), np.expand_dims(top_bboxes[:, 1],
+                                                                                                      -1), np.expand_dims(
+            top_bboxes[:, 2], -1), np.expand_dims(top_bboxes[:, 3], -1)
 
         # 去掉灰条
-        boxes = yolo_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([self.model_image_size[0],self.model_image_size[1]]),image_shape)
+        boxes = yolo_correct_boxes(top_ymin, top_xmin, top_ymax, top_xmax,
+                                   np.array([self.model_image_size[0], self.model_image_size[1]]), image_shape)
 
-        font = ImageFont.truetype(font='model_data/simhei.ttf',size=np.floor(3e-2 * np.shape(image)[1] + 0.5).astype('int32'))
+        font = ImageFont.truetype(font='model_data/simhei.ttf',
+                                  size=np.floor(3e-2 * np.shape(image)[1] + 0.5).astype('int32'))
 
         thickness = (np.shape(image)[0] + np.shape(image)[1]) // self.model_image_size[0]
 
@@ -164,7 +170,7 @@ class YOLO(object):
             label_size = draw.textsize(label, font)
             label = label.encode('utf-8')
             print(label)
-            
+
             if top - label_size[1] >= 0:
                 text_origin = np.array([left, top - label_size[1]])
             else:
@@ -177,7 +183,6 @@ class YOLO(object):
             draw.rectangle(
                 [tuple(text_origin), tuple(text_origin + label_size)],
                 fill=self.colors[self.class_names.index(predicted_class)])
-            draw.text(text_origin, str(label,'UTF-8'), fill=(0, 0, 0), font=font)
+            draw.text(text_origin, str(label, 'UTF-8'), fill=(0, 0, 0), font=font)
             del draw
         return image
-
